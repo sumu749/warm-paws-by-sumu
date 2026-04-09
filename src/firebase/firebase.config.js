132c5a91ef5172597438323b431241634a23b596
Sumu749 <sumaiya.cse.tec@gmail.com>
2026-04-10 01:42:11 +0600

    some changes added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBqWNxwT4L5KXBzZrfXfRUOj0uoa2ILFBI",
-    authDomain: "warm-paws-config.firebaseapp.com",
-    projectId: "warm-paws-config",
-    storageBucket: "warm-paws-config.firebasestorage.app",
-    messagingSenderId: "224825776402",
-    appId: "1:224825776402:web:32a30467054a126acbbbcb",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
